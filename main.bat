35ed99ec59fb45cb90c824ded81c4edc39611ef7
ElliotSu <sodinfeliz@gmail.com>
2021-03-09 10:27:07 +0800

    Removed .pyc files.

--- main.bat
+++ main.bat
@@ -1,4 +1,5 @@
 @echo off
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 set root=C:\Anaconda3
 set file_path=%~dp0main.py
 call %root%\Scripts\activate.bat %root%
--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from pathlib import Path
 from pkgs.mainGUI import mainGUI
 
 
 if __name__ == '__main__':
-    data_path = Path.cwd().joinpath('data')
     app = QApplication(sys.argv)
     app.setOverrideCursor(Qt.ArrowCursor)
-    widget = mainGUI(data_path)
+    widget = mainGUI()
     widget.show()
     app.exec_()
